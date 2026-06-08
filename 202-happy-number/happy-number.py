46eb570fcb454646d40daa29d2c804db0aae2786
Greeshma Polanki <vu.241fa04776@gmail.com>
2026-06-08 18:20:44 +0530

    Time: 3 ms (38.38%) | Memory: 19.1 MB (89.67%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -2,20 +2,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         seen = set()
-
         while n != 1:
             if n in seen:
                 return False
-
             seen.add(n)
             sum = 0
-
             while n > 0:
                 digit = n % 10
                 sum += digit * digit
                 n //= 10
-
             n = sum
-
         return True
 
